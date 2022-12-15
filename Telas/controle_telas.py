7b0db3dc56be60fec02f43a16d3293f2ac37faf6
mateus-dsm <116819046+mateus-dsm@users.noreply.github.com>
2022-12-15 14:28:09 -0300

    Integração Telas

--- Telas/controle_telas.py
+++ Telas/controle_telas.py
@@ -27,16 +27,24 @@ def chama_telaQuestion(id_tela_anterior):
 
 
 def chama_telaAnswers(id_tela_anterior):
+    if(id_tela_anterior == 4):
+        envia_pergunta()
+    elif(id_tela_anterior == 5):
+        envia_resposta()
     fecha_tela(id_tela_anterior)
     tela_answers.show()
 
 
-def chama_telaLogin(id_tela_anterior):
+def chama_telaLogin(id_tela_anterior, id_acao):
+    if(id_acao == 0):
+        envia_dados_telaNewUser(tela_new_user)
     fecha_tela(id_tela_anterior)
     tela_login.show()
-
+# id_acao: identifica qual acao foi feita para chegar nessa tela
 
 def chama_telaHome(id_tela_anterior):
+    if(id_tela_anterior == 1):
+        envia_dados_telaLogin(tela_login)
     fecha_tela(id_tela_anterior)
     tela_home.show()
 
@@ -45,6 +53,33 @@ def chama_telaNewUser(id_tela_anterior):
     fecha_tela(id_tela_anterior)
     tela_new_user.show()
 
+
+#funcoes para enviar e armazenar os dados das telas
+
+def envia_dados_telaLogin(self):
+    login = self.line_edit_login.text()
+    senha = self.line_edit_senha.text()
+    print("Login: ", login)
+    print("Senha: ", senha)
+
+def envia_dados_telaNewUser(self):
+    novo_login = self.line_edit_novo_login.text()
+    nova_senha = self.line_edit_nova_senha.text()
+    print("Novo login: ", novo_login)
+    print("Nova senha: ", nova_senha)
+
+def busca_pergunta(self):
+    pergunta = self.line_edit_busca.text()
+    print(pergunta)
+    self.line_edit_busca.clear()
+
+def envia_pergunta():
+    pergunta = tela_question.text_edit_pergunta.toPlainText()
+    print(pergunta)
+
+def envia_resposta():
+    resposta = tela_answers.text_edit_resposta.toPlainText()
+    print(resposta)
 
 # id telas:
 # login: 1
@@ -75,10 +110,14 @@ tela_login.button_enviar.clicked.connect(lambda: chama_telaHome(1))
 tela_login.button_novo_cadastro.clicked.connect(lambda: chama_telaNewUser(1))
 
 tela_home.button_nova_pergunta.clicked.connect(lambda: chama_telaQuestion(2))
-tela_home.button_sair.clicked.connect(lambda: chama_telaLogin(2))
+tela_home.button_sair.clicked.connect(lambda: chama_telaLogin(2, 1))
+#id_acao == 1 para o botao de voltar
+tela_home.line_edit_busca.returnPressed.connect(lambda: busca_pergunta(tela_home))
 
-tela_new_user.button_cadastrar.clicked.connect(lambda: chama_telaLogin(3))
-tela_new_user.button_cancelar.clicked.connect(lambda: chama_telaLogin(3))
+tela_new_user.button_cadastrar.clicked.connect(lambda: chama_telaLogin(3, 0))
+#id_acao == 0 para o botao de cadastrar
+tela_new_user.button_cancelar.clicked.connect(lambda: chama_telaLogin(3, 1))
+#id_acao == 1 para o botao de voltar
 
 tela_question.button_voltar.clicked.connect(lambda: chama_telaHome(4))
 tela_question.button_perguntar.clicked.connect(lambda: chama_telaAnswers(4))
